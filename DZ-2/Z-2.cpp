7d23b28aaa7ea385ddd8f1eb9ad5b5b2be52beec
dark1996 <kipin1996@gmail.com>
2022-03-23 17:49:27 +0300

    DZ-2

---
+++ DZ-2/Z-2.cpp
@@ -0,0 +1,34 @@
+﻿
+
+#include <stdio.h>
+
+int primenumber(int i)
+{
+    if (i % 2 == 0 )
+    {
+        printf("\n it's a hard number");
+        return 0;
+    }
+    else
+    {
+        if (i % 3 == 0)
+        {
+            printf("\n it's a hard number");
+            return 0;
+        }
+        else
+        {
+            printf("\n it's a prime number");
+            return 0;
+        }
+    }
+}
+
+int main(void)
+{
+    int a;
+    printf("Enter the number:");
+    scanf_s ("%d", &a);
+    primenumber(a);
+    return 0;
+}
