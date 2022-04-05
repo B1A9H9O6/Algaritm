9a723d5a98f0071aad8c53b8f9ddaadb344aba34
dark1996 <kipin1996@gmail.com>
2022-04-05 21:05:23 +0300

    ispr

--- DZ-2/Ispr.cpp
+++ DZ-2/Ispr.cpp
@@ -4,14 +4,19 @@
 
 int primenumber(int i)
 {
-    if (i % 2 == 0 )
+    if (i % 2 == 0)
+    {
+        printf("\n it's a hard number");
+        return 0;
+    }
+    else if (i % 3 == 0)
     {
         printf("\n it's a hard number");
         return 0;
     }
     else
     {
-       
+
         printf("\n it's a prime number");
         return 0;
     }
@@ -21,7 +26,7 @@ int main(void)
 {
     int a;
     printf("Enter the number:");
-    scanf_s ("%d", &a);
+    scanf_s("%d", &a);
     primenumber(a);
     return 0;
 }
