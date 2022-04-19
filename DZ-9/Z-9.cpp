9afe438689d48fb7782daae702af565e15a3c62e
dark1996 <kipin1996@gmail.com>
2022-04-19 22:14:49 +0300

    DZ-9

---
+++ DZ-9/Z-9.cpp
@@ -0,0 +1,81 @@
+﻿#include <string.h>
+#include <stdio.h> 
+#include <stdlib.h>
+
+const int n = 6;
+int cursor = -1;
+int stack[n];
+int push(int data)
+{
+	if (cursor < n)
+	{
+		
+		stack[++cursor] = data;
+
+		return true;
+	}
+	else
+	{
+		printf("\nSteck overflow");
+		return false;
+	}
+}
+int pop()
+{
+
+	
+
+		return printf("%c,", stack[cursor--] + 65);
+	
+
+}
+
+
+int matrix[n][n] = {
+	{0,1,0,0,0,0},
+	{0,0,1,0,0,0},
+	{0,0,0,1,0,0},
+	{0,0,0,0,1,0},
+	{0,0,0,0,0,1},
+	{0,0,0,0,1,0},
+};
+
+int visited[n] = { 0 };
+
+void dep(int st)
+{
+	int r;
+	
+	printf("%c ", st +65);
+	visited[st] = 1;
+	for (r = 0; r < n; r++)
+	{
+		if (matrix[st][r] == 1 && !visited[r])
+		{
+			push(r);
+			dep(r);
+			
+		}
+	}
+	
+}
+
+void reset()
+{
+	for (int i = 0; i < n; i++)
+	{
+		visited[i] = 0;
+	}
+}
+int main()
+{
+	dep(0); 
+	reset();
+	printf("\n");
+	for (int i = 0; i < 6; i++)
+	{
+		pop();
+	}
+	
+	return 0;
+}
